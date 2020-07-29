1eb2dd20e63094f0a20c46782e1bbcc13acdad3d
Alan Cleator <alancleator@hotmail.com>
2020-07-29 16:52:51 +0100

    add util to wsgi classpath

--- acnet/wsgi.py
+++ acnet/wsgi.py
@@ -12,6 +12,7 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 sys.path.append("/home/pi/virtualenvs/acnetv/acnet")
+sys.path.append("/home/pi/python")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acnet.settings')
 
