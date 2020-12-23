a217c127206ccfbd5668c501076400f2423d3b43
Alan Cleator <alancleator@hotmail.com>
2020-12-23 12:29:23 +0000

    virtual host changes

--- league/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import League, Pl, Ch, Td
-admin.site.register(League)
-admin.site.register(Pl)
-admin.site.register(Ch)
-admin.site.register(Td)
